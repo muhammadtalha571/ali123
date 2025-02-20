d63dac97363ba8b774bcf5388c81447a1de4dcd5
Muhammad Talha <talhaakram070704@gmail.com>
2025-02-20 03:58:56 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+
+function getRandomColor() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+const randomColor = getRandomColor();
+
+
+console.log('Random RGB Color:', randomColor);
+
